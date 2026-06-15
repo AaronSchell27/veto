a858eaedfc5cce07247bc900caedb61582e56d07
Aaron Schell <aaron.schell@students.pcci.edu>
2026-06-15 16:32:51 -0500

    Removed unnecessary comment

--- veto/lib/bootstrap.dart
+++ veto/lib/bootstrap.dart
@@ -35,7 +35,5 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
 
   Bloc.observer = const AppBlocObserver();
 
-  // Add cross-flavor configuration here
-
   runApp(await builder());
 }
